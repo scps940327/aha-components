75b591c07a1cc205ec2823d6f36611666e6ba1e7
scps940327 <scps940327@gmail.com>
2024-05-29 03:15:23 +0800

    Add react-hook-form for control form value in SearchPage

--- src/pages/posts/SearchPage.tsx
+++ src/pages/posts/SearchPage.tsx
@@ -4,12 +4,13 @@ import Input from "components/Input";
 import Page from "components/Page";
 import SectionTitle from "components/SectionTitle";
 import Slider from "components/Slider";
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import THEME from "helpers/theme";
-import { perPageCountConfigs } from "./constants";
+import { perPageCountConfigs, searchFormKey } from "./constants";
 import WrapperWithFollowList from "./WrapperWithFollowList";
+import { useForm } from "react-hook-form";
 
 const SearchButton = styled(Button.Normal)`
   width: 343px;
@@ -22,32 +23,51 @@ const SearchButton = styled(Button.Normal)`
 const DEFAULT_PER_PAGE_COUNT_CONFIG_INDEX = 4;
 
 const SearchPage = () => {
-  const [count, setCount] = useState(perPageCountConfigs[DEFAULT_PER_PAGE_COUNT_CONFIG_INDEX].value);
-  const [keyword, setKeyword] = useState('');
+  const { register, setValue, watch } = useForm({
+    defaultValues: {
+      [searchFormKey.COUNT]: perPageCountConfigs[DEFAULT_PER_PAGE_COUNT_CONFIG_INDEX].value,
+      [searchFormKey.KEYWORD]: '',
+    }
+  });
+  const [countValue, keywordValue] = watch([searchFormKey.COUNT, searchFormKey.KEYWORD]);
+
+  useEffect(() => {
+    register(searchFormKey.COUNT);
+  }, [register]);
 
   return (
     <Page>
       <WrapperWithFollowList>
         <Box borderBottom="1px solid rgba(255, 255, 255, 0.1)" paddingBottom="30px">
           <SectionTitle title="Search" />
-          <Input placeholder="Keyword" variant="outlined" fullWidth onChange={(e) => setKeyword(e.target.value)} />
+          <Input
+            {...register(searchFormKey.KEYWORD)}
+            placeholder="Keyword"
+            variant="outlined"
+            fullWidth
+            // onChange={(e) => setKeyword(e.target.value)}
+          />
         </Box>
         <Box borderBottom="1px solid rgba(255, 255, 255, 0.1)" paddingBottom="30px">
           <SectionTitle title="# of results per page" />
           <Box display="flex" alignItems="baseline">
-            <Box fontSize="48px" fontWeight="700" marginRight="10px">{count}</Box>
+            <Box fontSize="48px" fontWeight="700" marginRight="10px">{countValue}</Box>
             <Box fontSize="16px">results</Box>
           </Box>
           <Slider
             marks={perPageCountConfigs.map(({ label }, index) => ({ label, value: index }))}
             getAriaValueText={(value, index) => perPageCountConfigs[index].label}
             step={null}
-            max={perPageCountConfigs.length - 1}
             defaultValue={DEFAULT_PER_PAGE_COUNT_CONFIG_INDEX}
-            onChange={(e, value) => setCount(perPageCountConfigs[value as number].value)}
+            min={undefined}
+            max={perPageCountConfigs.length - 1}
+            onChange={(e, value) => setValue(
+              searchFormKey.COUNT,
+              perPageCountConfigs[value as number].value
+            )}
           />
         </Box>
-        <Link to={`post/list?keyword=${keyword}&count=${count}`}>
+        <Link to={`post/list?keyword=${keywordValue}&count=${countValue}`}>
           <SearchButton>SEARCH</SearchButton>
         </Link>
       </WrapperWithFollowList>
